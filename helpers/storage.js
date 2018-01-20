6d4e55324ca4a590a7ec16b5a14f359b46b5472d
GunshipPenguin <me@rhysre.net>
2018-01-20 03:45:45 -0500

    Document storage.js

--- helpers/storage.js
+++ helpers/storage.js
@@ -7,6 +7,13 @@ const utils = lib.utils({
 })
 
 const storage = {
+  /**
+   * Retrieves the game hosted by the host with hostPhoneNumber from
+   * persistent storage.
+   *
+   * @param {string} hostPhoneNumber Phone number of host of game to retrieve
+   * @return {promise} a promise that resolves with game information
+   */
   getGame: function (hostPhoneNumber) {
     return new Promise((resolve, reject) => {
       lib.utils.storage.get(hostPhoneNumber, (err, value) => {
@@ -21,11 +28,16 @@ const storage = {
     })
   },
 
+  /**
+   * Sets the game information for the game hosted by the host with
+   * hostPhoneNumber to the specified object.
+   *
+   * @param {string} hostPhoneNumber Phone number of host of game to set
+   * @param {object} gameInfo Object containing game information to set
+   */
   setGame: function (hostPhoneNumber, gameInfo) {
     lib.utils.storage.set(hostPhoneNumber, gameInfo, (err, value) => {
-      if (err) {
-        utils.log.error('Could not get game', err)
-      }
+      if (err) utils.log.error('Could not get game', err)
     })
   }
 }
