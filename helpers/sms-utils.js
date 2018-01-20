fdc30766799f0dbb729406854e3bc55299cd53e5
GunshipPenguin <me@rhysre.net>
2018-01-20 02:55:26 -0500

    Add SMS parsing utilities

---
+++ helpers/sms-utils.js
@@ -0,0 +1,28 @@
+const zlib = require('zlib')
+const tokens = require('../tokens')
+const lib = require('lib')({token: tokens.STDLIB_TOKEN})
+const utils = lib.utils({
+  service: 'text-game'
+})
+
+const smsUtils = {
+  /**
+   * Given a message as base64 encoded zlib data, return its corresponding
+   * JSON.
+   *
+   * @param {string} message Message to decode
+   */
+  decodeMessage: function (message) {
+    try {
+      let decompressed = zlib.inflateSync(Buffer.from(message, 'base64'))
+      console.log(decompressed.toString())
+      let parsedObject = JSON.parse(decompressed.toString())
+
+      return parsedObject
+    } catch (e) {
+      utils.log.error('Could not parse SMS', e)
+    }
+  }
+}
+
+module.exports = smsUtils
