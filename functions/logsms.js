1ec7d53c1f2af7ea00773017bdd9a625ef4e6c23
GunshipPenguin <me@rhysre.net>
2018-01-20 20:00:12 -0500

    Clean up logsms.js

--- functions/logsms.js
+++ functions/logsms.js
@@ -1,13 +1,14 @@
 'use strict'
+
 const tokens = require('../tokens')
 const smsUtils = require('../helpers/sms-utils')
-const lib = require('lib')({token: 'IyOStmVa0I-f5r1DNhJB2i5Cykyem2f6wzaBxzBdJM-59DLTGVe2oXA-jMEAr638'})
+const lib = require('lib')({token: process.env.STDLIB_TOKEN})
 const utils = lib.utils({
   service: 'text-game'
 })
 
 /**
-* Log all incoming sms messages 
+* Logs incoming SMS Messages for testing purposes
 *
 * @param {string} sender The phone number that sent the text to be handled
 * @param {string} receiver The StdLib phone number that received the SMS
@@ -18,7 +19,6 @@ module.exports = (sender, receiver, message, createdDatetime, context, callback)
   // Decompress + decode message
   let messageJson = smsUtils.decodeMessage(message)
 
-  utils.log("Got text", {raw: message, decoded: messageJson})
-      .then(callback)
-      .catch(callback)
+  utils.log(`Got text from ${sender}`, {raw: message, decoded: messageJson}).then(callback)
+  .catch(callback)
 }
